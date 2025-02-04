6468719f783d95f4fc1bc57f0a25bb932b8299a6
ladylavandula <fatoushisawah@gmail.com>
2025-02-03 20:07:46 -0800

    Initial commit

---
+++ src/main/java/com/lavandula/lavglen/LavGlenClient.java
@@ -0,0 +1,10 @@
+package com.lavandula.lavglen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LavGlenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
